ed5acb7f5853382f7cbc7ce7bc18cc71d460972e
Sasu Karttunen <sasu.karttunen@tpnet.fi>
2011-06-23 14:54:11 +0300

    removed accidentally added files

--- src/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import dbus
- 
-bus = dbus.SessionBus()
-helloservice = bus.get_object('org.pywapi.Weather', '/Weather')
-condition = helloservice.get_dbus_method('condition', 'org.pywapi.Weather')
-print condition()
